c4ff79d5ff4b98a4a7cccc066aa558b8d98b8385
Artem Vysochyn <artem.vysochyn@gmail.com>
2020-05-09 15:19:01 +0300

    Added more scripts

--- def_access.simple.sh
+++ def_access.simple.sh
@@ -2,14 +2,14 @@ set -u
 
 ACCESS_ID=$1
 TEMPLATE_DATA=$2
-KEY=$ACCESS_ID
-ROLE=$ACCESS_ID
+KEY=$ACCESS_ID-id-key
+ROLE=$ACCESS_ID-id-role
 
 echo "*** granting access: [$ACCESS_ID]"
 
 # Register access_request policy which allows to generate oidc token.
 echo "path \"identity/oidc/token/$ACCESS_ID\" {capabilities=[\"create\", \"read\"]}" > payload.hcl
-vault policy write $ACCESS_ID payload.hcl
+vault policy write $ACCESS_ID-id-token-policy payload.hcl
 
 # Create oidc/role
 echo "{\"key\": \"$KEY\", \"template\": \"$TEMPLATE_DATA\"}" > payload.json
