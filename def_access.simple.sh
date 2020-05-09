7048c07d47d9e5ee41e75d91d68c08f1494237bd
Artem Vysochyn <artem.vysochyn@gmail.com>
2020-05-09 14:59:29 +0300

    Added more scripts

---
+++ def_access.simple.sh
@@ -0,0 +1,22 @@
+set -u
+
+ACCESS_ID=$1
+TEMPLATE_DATA=$2
+KEY=$ACCESS_ID
+ROLE=$ACCESS_ID
+
+echo "*** granting access: [$ACCESS_ID]"
+
+# Register access_request policy which allows to generate oidc token.
+echo "path \"identity/oidc/token/$ACCESS_ID\" {capabilities=[\"create\", \"read\"]}" > payload.hcl
+vault policy write $ACCESS_ID payload.hcl
+
+# Create oidc/role
+echo "{\"key\": \"$KEY\", \"template\": \"$TEMPLATE_DATA\"}" > payload.json
+curl --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data @payload.json $VAULT_ADDR/v1/identity/oidc/role/$ROLE
+
+# Create oidc/key
+echo "{\"allowed_client_ids\": \"*\", \"verification_ttl\": 0, \"rotation_period\": \"1m\"}" > payload.json
+curl --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data @payload.json $VAULT_ADDR/v1/identity/oidc/key/$KEY
+
+echo "*** access granted!"
